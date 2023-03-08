8508cc989f405523e958167e3023aaa5c204d1d9
Sabal Shrestha <sabalshrestha427@gmail.com>
2023-03-08 11:11:17 +0545

    Back to top button added

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const header = document.getElementById("header");
+const mybutton = document.getElementById("goToTop");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 30) {
     header.classList.add("scrolled");
+    mybutton.style.bottom = "40px";
   } else {
     header.classList.remove("scrolled");
+    mybutton.style.bottom = "-50px";
   }
 });
+
+function goToTop() {
+  (document.body.scrollTop = 0), (document.documentElement.scrollTop = 0);
+}
