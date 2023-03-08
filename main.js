757f31430759528a92c831990224311bef4efe68
Sabal Shrestha <sabalshrestha427@gmail.com>
2023-03-08 12:13:15 +0545

    Something flashy on Stage 2 page

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const mybutton = document.getElementById("goToTop");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 30) {
     header.classList.add("scrolled");
-    mybutton.style.bottom = "40px";
+    mybutton.style.bottom = "4%";
   } else {
     header.classList.remove("scrolled");
     mybutton.style.bottom = "-50px";
